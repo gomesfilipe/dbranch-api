03e6c728a515379ea7431558f713d0939b8c46bb
Filipe <filipejvmf@gmail.com>
2024-04-26 22:23:40 -0300

    feat: add cast to algorithm field

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Algorithm;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,9 @@ class Run extends Model
         'instance',
         'value',
         'algorithm',
+    ];
+
+    protected $casts = [
+        'algorithm' => Algorithm::class,
     ];
 }
